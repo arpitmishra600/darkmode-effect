d79815140b933c55093783245cfd8a536da3d79b
Arpit Mishra <mishraarpit600@gmail.com>
2023-06-22 19:27:40 +0530

    skeletal structure

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-// const getbyid=(str,callback)=>{
-//     document.getElementById(str).onclick(()=>{
-//         callback()
-//     })
-// }
+const getbyid=(id,str)=>{
+    document.getElementById(id).style.animation=str;
+}
 
 const anm_clr=(id)=>{
     setTimeout(() => {
-      document.getElementById(id).style.animation=""
-    }, 1500);
+      getbyid(id,"")
+    }, 2300);
 }
 var flag=true
 document.getElementById("btn").addEventListener("click",()=>{
     if(flag==true){
-        document.getElementById("entbarid1").style.animation="scale-up-hor-center 1.5s cubic-bezier(0.550, 0.055, 0.675, 0.190) both"
+        getbyid("entbarid1","scale-up-hor-center 1.5s cubic-bezier(0.550, 0.055, 0.675, 0.190) both")
+        setTimeout(() => {
+            getbyid("celestialid","bounce-in-top 0.6s both")
+        }, 800);
+        setTimeout(() => {
+            getbyid("celestialid","slide-out-fwd-center 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both")
+        }, 1800);
         anm_clr("entbarid1")
         flag=false;
     }
     else{
-        document.getElementById("entbarid2").style.animation="scale-up-hor-center 1.5s cubic-bezier(0.550, 0.055, 0.675, 0.190) both"
-        anm_clr("entbarid2")
+        getbyid("celestialid","slide-in-fwd-center 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both")
+        setTimeout(() => {
+            getbyid("celestialid","bounce-out-bottom 1.5s both")
+        }, 500);
         flag=true;
     }
    
